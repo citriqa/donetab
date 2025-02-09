49a3f7386f8c67545d96e89bd53713bc41c059d0
citriqa <git-noreply@citri.qa>
2025-02-09 15:59:52 -0600

    chore: add keys to manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,6 +6,14 @@ import { defineConfig, WxtViteConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	manifest: {
+		name: "DoneTab",
+		description: "Rapidly save and restore windows",
+		homepage_url: "https://github.com/citriqa/donetab",
+		browser_specific_settings: {
+			gecko: {
+				id: "donetab@citri.qa",
+			},
+		},
 		action: { // action property is needed to enable toolbar click events
 			"default_title": "DoneTab - double click to save window",
 		},
