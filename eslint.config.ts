54390e29f0b260e1b4080ddc334cc19326a0b001
citriqa <git-noreply@citri.qa>
2025-02-18 12:51:31 -0600

    chore(eslint): add react-{compiler,hooks} plugins

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,10 @@
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-assignment -- will be upgraded to typescript soon
+const pluginReactHooks = require("eslint-plugin-react-hooks");
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-assignment -- will be upgraded to typescript soon
+const pluginReactCompiler = require("eslint-plugin-react-compiler");
 
 export default tseslint.config(
 	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -15,6 +19,20 @@ export default tseslint.config(
 	...tseslint.configs.strictTypeChecked,
 	pluginReact.configs.flat.recommended,
 	pluginReact.configs.flat["jsx-runtime"],
+	{
+		plugins: {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			"react-hooks": pluginReactHooks,
+		},
+		rules: {
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "warn",
+		},
+		extends: [
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+			pluginReactCompiler.configs.recommended,
+		],
+	},
 	{
 		languageOptions: {
 			parserOptions: {
